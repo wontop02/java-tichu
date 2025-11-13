24ff11889ac022adc5c50275e5c540ee845fffb1
wontop02 <wjy0502kr@naver.com>
2025-11-14 01:13:23 +0900

    feat: 참가자 이름 검증 기능 구현

---
+++ src/main/java/tichu/util/InputValidator.java
@@ -0,0 +1,42 @@
+package tichu.util;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class InputValidator {
+    private static final String BLANK_INPUT = "빈 문자열 또는 공백은 입력할 수 없습니다.";
+    private static final String NOT_ENGLISH_AND_KOREAN_ONLY = "한글(자음 모음 조합)과 영어만 입력이 가능합니다.";
+    private static final String DUPLICATED = "중복된 입력은 허용하지 않습니다.";
+    private static final String ENGLISH_AND_KOREAN_ONLY_REGEX = "^[가-힣a-zA-Z]*$";
+
+    private InputValidator() {
+    }
+
+    public static void validatePlayerName(String input) {
+        validateNotBlank(input);
+        List<String> inputs = validateNotDuplicate(input);
+        inputs.forEach(InputValidator::validateEnglishAndKoreanOnly);
+    }
+
+    private static void validateNotBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_INPUT);
+        }
+    }
+
+    private static List<String> validateNotDuplicate(String input) {
+        List<String> inputs = Arrays.asList(input.split(",", -1));
+        if (new HashSet<>(inputs).size() != inputs.size()) {
+            throw new IllegalArgumentException(DUPLICATED);
+        }
+        return inputs;
+    }
+
+    private static void validateEnglishAndKoreanOnly(String input) {
+        if (!input.matches(ENGLISH_AND_KOREAN_ONLY_REGEX)) {
+            throw new IllegalArgumentException(NOT_ENGLISH_AND_KOREAN_ONLY);
+        }
+    }
+
+}
---
+++ src/test/java/tichu/util/InputValidatorTest.java
@@ -0,0 +1,28 @@
+package tichu.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class InputValidatorTest {
+
+    @DisplayName("한글(자음 모음 조합), 영어를 제외한 문자가 있으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"ㅁ", "1", "h i", "!"})
+    void 한글과_영어를_제외한_문자가_있으면_예외가_발생한다(String input) {
+        assertThatThrownBy(() -> InputValidator.validatePlayerName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("한글(자음 모음 조합)과 영어만 입력이 가능합니다.");
+    }
+
+    @DisplayName("중복된 입력이 존재하면 예외가 발생한다.")
+    @Test
+    void 중복된_입력이_존재하면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validatePlayerName("철수,영희,철수"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 입력은 허용하지 않습니다.");
+    }
+}
