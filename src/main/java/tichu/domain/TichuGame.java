0e03d20acf8b263900f91040b24696e035d3aab7
wontop02 <wjy0502kr@naver.com>
2025-11-14 21:22:44 +0900

    feat: 참가자가 4명이 아닐 경우 예외 발생, 이름으로 참가자 찾는 기능 구현

--- src/main/java/tichu/domain/TichuGame.java
+++ src/main/java/tichu/domain/TichuGame.java
@@ -5,14 +5,33 @@ import java.util.Collections;
 import java.util.List;
 
 public class TichuGame {
-    private final List<Player> direction = new ArrayList<>();
+    private static final String INCORRECT_PLAYER_NUMBER = "참가자 수는 반드시 4명이어야 합니다.";
+    private static final String PLAYER_NOT_FOUND = "해당 이름의 플레이어를 찾을 수 없습니다: ";
 
+    private final List<Player> players;
+    private final List<Player> direction = new ArrayList<>();
     private final List<Player> red = new ArrayList<>();
     private final List<Player> blue = new ArrayList<>();
 
     public TichuGame(List<Player> players) {
+        validatePlayers(players);
+        this.players = players;
         decisionDirection(players);
         decisionTeams();
+    }
+
+    public Player findPlayerByName(String name) {
+        return players.stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalArgumentException(PLAYER_NOT_FOUND + name));
+    }
+
+    private void validatePlayers(List<Player> players) {
+        if (players.size() != 4) {
+            throw new IllegalArgumentException(INCORRECT_PLAYER_NUMBER);
+        }
     }
 
     private void decisionDirection(List<Player> players) {
