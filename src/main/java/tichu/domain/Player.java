d81cad5b01aed5a98ab409899d9efd9050c41ebe
wontop02 <wjy0502kr@naver.com>
2025-11-16 17:00:41 +0900

    feat: 플레이어가 보유한 카드인지 검증하는 기능 구현

--- src/main/java/tichu/domain/Player.java
+++ src/main/java/tichu/domain/Player.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Player {
+    private static final String NOT_IN_MY_CARDS = "보유하지 않은 카드가 존재합니다.";
     private final String name;
     private List<Card> myCards = new ArrayList<>();
     private List<Card> acquiredCards = new ArrayList<>();
@@ -15,6 +16,12 @@ public class Player {
 
     public void addMyCards(List<Card> card) {
         myCards.addAll(card);
+    }
+
+    public void validateContainMyCard(Card card) {
+        if (!myCards.contains(card)) {
+            throw new IllegalArgumentException(NOT_IN_MY_CARDS);
+        }
     }
 
     public void removeMyCards(List<Card> cards) {
