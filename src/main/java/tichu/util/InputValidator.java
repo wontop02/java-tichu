18719c0d7c3546e7b792b314d41be18256ba3132
wontop02 <wjy0502kr@naver.com>
2025-11-20 04:33:35 +0900

    feat: CallRank 입력을 검증하는 기능 구현

--- src/main/java/tichu/util/InputValidator.java
+++ src/main/java/tichu/util/InputValidator.java
@@ -8,11 +8,12 @@ public class InputValidator {
     private static final String BLANK_INPUT = "빈 문자열 또는 공백은 입력할 수 없습니다.";
     private static final String NOT_ENGLISH_AND_KOREAN_ONLY = "한글(자음 모음 조합)과 영어만 입력이 가능합니다.";
     private static final String DUPLICATED = "중복된 입력은 허용하지 않습니다.";
-    private static final String INVALID_CARD_FORMAT = "카드 형식이 올바르지 않습니다.";
+    private static final String INVALID_CARD_FORMAT = "카드 입력 형식(숫자/알파벳+문양[s,d,h,c])이 올바르지 않습니다.";
 
     private static final String ENGLISH_AND_KOREAN_ONLY_REGEX = "^[가-힣a-zA-Z]+$";
     private static final String NORMAL_CARD_FORMAT = "^(2|3|4|5|6|7|8|9|10|j|J|q|Q|k|K|a|A)[sdhcSDHC]$";
     private static final String SPECIAL_CARD_FORMAT = "^(1|개|봉|용)$";
+    private static final String CALL_RANK_FORMAT = "^(2|3|4|5|6|7|8|9|10|j|J|q|Q|k|K|a|A)$";
 
     private InputValidator() {
     }
@@ -33,6 +34,13 @@ public class InputValidator {
         validateNotBlank(input);
         List<String> inputs = validateNotDuplicate(input);
         inputs.forEach(InputValidator::validateCardFormat);
+    }
+
+    public static void validateCallRank(String input) {
+        validateNotBlank(input);
+        if (!input.matches(CALL_RANK_FORMAT)) {
+            throw new IllegalArgumentException(INVALID_CARD_FORMAT);
+        }
     }
 
     private static void validateNotBlank(String input) {
