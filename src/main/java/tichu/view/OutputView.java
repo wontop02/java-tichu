05cb3ffa4810869bcfd9f7ba6e4a2844ab43c8dc
wontop02 <wjy0502kr@naver.com>
2025-11-20 04:34:36 +0900

    feat: 카드를 console에 출력하는 기능 구현

---
+++ src/main/java/tichu/view/OutputView.java
@@ -0,0 +1,12 @@
+package tichu.view;
+
+import java.util.List;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printCards(List<String> cards) {
+        System.out.println(cards);
+    }
+}
