3a1c78663fd73b980f01c7b5a107fadda8be1060
wontop02 <wjy0502kr@naver.com>
2025-11-20 04:34:15 +0900

    feat: combination과 callRank를 요청하는 기능 구현

--- src/main/java/tichu/view/InputView.java
+++ src/main/java/tichu/view/InputView.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String REQUEST_PLAYERS_NAME = "참가자 4명의 이름을 입력해 주세요(쉼표로 구분): ";
-    private static final String REQUEST_TICHU_CALLER_NAME = "%s 티츄를 부를 참가자 이름을 입력해 주세요(쉼표로 구분): ";
-    private static final String REQUEST_TRADE_CARD = "%s에게 줄 카드를 입력해 주세요: ";
+    private static final String REQUEST_TICHU_CALLER_NAME = "%s 티츄를 부를 참가자 이름을 입력해 주세요(쉼표로 구분, 없으면 x): ";
+    private static final String REQUEST_TRADE_CARD = "%s님에게 줄 카드를 입력해 주세요: ";
+    private static final String REQUEST_COMBINATION = "[%s님 차례] 낼 조합을 선택해 주세요(쉼표로 구분, 패스는 p): ";
+    private static final String REQUEST_CALL_RANK = "콜을 부를 숫자 또는 알파벳을 입력해 주세요(문양 제외 입력, 없으면 x): ";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -24,6 +26,16 @@ public class InputView {
 
     public static String requestTradeCard(String toPlayerName) {
         System.out.printf(REQUEST_TRADE_CARD, toPlayerName);
+        return scanner.nextLine();
+    }
+
+    public static String requestCombination(String playerName) {
+        System.out.printf(REQUEST_COMBINATION, playerName);
+        return scanner.nextLine();
+    }
+
+    public static String requestCallRank() {
+        System.out.printf(REQUEST_CALL_RANK);
         return scanner.nextLine();
     }
 }
