3c6b65d30b6d6cdf3e8e4d43c65fdf2de69f0bcd
wontop02 <wjy0502kr@naver.com>
2025-11-21 06:34:40 +0900

    feat: 폭탄 낼 사람, 폭탄 조합 입력 받는 기능 구현

--- src/main/java/tichu/view/InputView.java
+++ src/main/java/tichu/view/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
     private static final String REQUEST_COMBINATION = "[%s님 차례] 낼 조합을 선택해 주세요(쉼표로 구분, 패스는 p): ";
     private static final String REQUEST_CALL_RANK = "콜을 부를 숫자 또는 알파벳을 입력해 주세요(문양 제외 입력, 없으면 x): ";
     private static final String REQUEST_RECEIVE_PLAYER_NAME = "용으로 획득한 카드를 받을 상대 팀 플레이어 이름을 입력해 주세요: ";
+    private static final String REQUEST_BOMB_PLAYER_NAME = "폭탄을 낼 참가자 이름을 입력해 주세요(없으면 x): ";
+    private static final String REQUEST_BOMB_COMBINATION = "폭탄을 입력해 주세요";
 
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -32,6 +34,16 @@ public class InputView {
 
     public static String requestCombination(String playerName) {
         System.out.printf(REQUEST_COMBINATION, playerName);
+        return scanner.nextLine();
+    }
+
+    public static String requestBombPlayer() {
+        System.out.printf(REQUEST_BOMB_PLAYER_NAME);
+        return scanner.nextLine();
+    }
+
+    public static String requestBomb() {
+        System.out.printf(REQUEST_BOMB_COMBINATION);
         return scanner.nextLine();
     }
 
