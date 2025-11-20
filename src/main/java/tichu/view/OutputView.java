3c8f16da4bd7ddaa3ef4956d4dcafb2cbc21d48e
wontop02 <wjy0502kr@naver.com>
2025-11-20 20:43:10 +0900

    feat: 예외 메시지를 출력하는 기능 구현

--- src/main/java/tichu/view/OutputView.java
+++ src/main/java/tichu/view/OutputView.java
@@ -3,10 +3,17 @@ package tichu.view;
 import java.util.List;
 
 public class OutputView {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_SUFFIX = " 다시 입력해 주세요.";
+
     private OutputView() {
     }
 
     public static void printCards(List<String> cards) {
         System.out.println(cards);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(ERROR_PREFIX + message + ERROR_SUFFIX);
     }
 }
