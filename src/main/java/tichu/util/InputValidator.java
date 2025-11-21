7b98cb24397a8ddcfaca04cd99bc7bdacbffe6b1
wontop02 <wjy0502kr@naver.com>
2025-11-21 09:47:56 +0900

    feat: 예약어는 이름으로 설정할 수 없도록 검증 추가

--- src/main/java/tichu/util/InputValidator.java
+++ src/main/java/tichu/util/InputValidator.java
@@ -9,7 +9,10 @@ public class InputValidator {
     private static final String NOT_ENGLISH_AND_KOREAN_ONLY = "한글(자음 모음 조합)과 영어만 입력이 가능합니다.";
     private static final String DUPLICATED = "중복된 입력은 허용하지 않습니다.";
     private static final String INVALID_CARD_FORMAT = "카드 입력 형식(숫자/알파벳+문양[s,d,h,c])이 올바르지 않습니다.";
+    private static final String RESERVED_WORD = "프로그램 진행 시 필요한 예약어(x,p)를 포함할 수 없습니다.";
 
+    private static final String PASS_RESERVED_WORD = "p";
+    private static final String CANCEL_RESERVED_WORD = "x";
     private static final String ENGLISH_AND_KOREAN_ONLY_REGEX = "^[가-힣a-zA-Z]+$";
     private static final String NORMAL_CARD_FORMAT = "^(2|3|4|5|6|7|8|9|10|j|J|q|Q|k|K|a|A)[sdhcSDHC]$";
     private static final String SPECIAL_CARD_FORMAT = "^(1|개|봉|용)$";
@@ -22,6 +25,7 @@ public class InputValidator {
         validateNotBlank(input);
         List<String> inputs = validateNotDuplicate(input);
         inputs.forEach(InputValidator::validateEnglishAndKoreanOnly);
+        validateReservedWord(inputs);
     }
 
     public static void validateTichuCallerName(String input) {
@@ -64,6 +68,14 @@ public class InputValidator {
     private static void validateEnglishAndKoreanOnly(String input) {
         if (!input.matches(ENGLISH_AND_KOREAN_ONLY_REGEX)) {
             throw new IllegalArgumentException(NOT_ENGLISH_AND_KOREAN_ONLY);
+        }
+    }
+
+    private static void validateReservedWord(List<String> inputs) {
+        for (String input : inputs) {
+            if (input.equals(PASS_RESERVED_WORD) || input.equals(CANCEL_RESERVED_WORD)) {
+                throw new IllegalArgumentException(RESERVED_WORD);
+            }
         }
     }
 
