12e0231f4111f0d6a7ce611b6b287327d3842428
wontop02 <wjy0502kr@naver.com>
2025-11-23 07:48:50 +0900

    refactor: 입력 요청 메시지 수정

--- src/main/java/tichu/view/InputView.java
+++ src/main/java/tichu/view/InputView.java
@@ -5,7 +5,7 @@ import tichu.dto.PlayerDto;
 
 public class InputView {
     private static final String REQUEST_PLAYERS_NAME = "참가자 4명의 이름을 입력해 주세요(쉼표로 구분): ";
-    private static final String REQUEST_TICHU_CALLER_NAME = "%s 티츄를 부를 참가자 이름을 입력해 주세요(쉼표로 구분, 없으면 x): ";
+    private static final String REQUEST_TICHU_CALLER_NAME = "\n%s 티츄를 부를 참가자 이름을 입력해 주세요(쉼표로 구분, 없으면 x): ";
     private static final String REQUEST_TRADE_CARD = "%s님에게 줄 카드를 입력해 주세요: ";
     private static final String REQUEST_COMBINATION = "낼 조합을 선택해 주세요(쉼표로 구분, 패스는 p): ";
     private static final String REQUEST_CALL_RANK = "콜을 부를 숫자 또는 알파벳을 입력해 주세요(문양 제외 입력, 없으면 x): ";
